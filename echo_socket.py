bda241833ba8c32d8f6bc90bde96529d5f59e549
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-12-01 19:15:27 -0500

    Changing around the button names, labels, etc. to comply with android best-practices, and to line up with what's currently being used by the app in master.

--- echo_socket.py
+++ echo_socket.py
@@ -5,6 +5,8 @@ the person who wrote "https://docs.python.org/2/howto/sockets.html" is a great h
 
 import socket
 import sys
+import rospy
+from std_msgs.msg import String, Float64
 
 class EchoSocket(object):
 	"""
@@ -14,12 +16,16 @@ class EchoSocket(object):
 	def __init__(self, port=8888):
 		#creates a socket with INET protocol and streaming; gets this computer's address; uses an arbitrary port; binds socket to the address and port.
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# self.host = socket.gethostbyname(socket.gethostname())
 		self.host = ''
 		self.port = port #this is an arbitrary number right now.
 		print "Binding socket to ", self.host, " : ", self.port
 		self.sock.bind((self.host, self.port))
 		self.sock.listen(5)
+		rospy.init_node("/android_socket")
+		self.button_pub = rospy.Publisher("/android_buttons", String, queue_size=10)
+		self.orientation_pub = rospy.Publisher("/android_yaw", Float64, queue_size = 10)
+        # rospy.Subscriber("/wii_rumble", Float32, self.set_rumble)
+
 
 	def read_socket(self, msg_bytes):
 		"""
@@ -31,17 +37,24 @@ class EchoSocket(object):
 			break
 		chunks = []
 		received_bytes = 0
-		# print repr(msg_bytes)
-		# print "test"
 		while received_bytes < msg_bytes:
-			# print received_bytes, msg_bytes
 			chunk = c.recv(min(msg_bytes - received_bytes, 2048))
 			if chunk == '':
 				break
-				# raise RuntimeError("Socket connection broken.")
 			chunks.append(chunk)
 			received_bytes += len(chunk)
 		msg = ''.join(chunks)
+		if msg[:3] == "Yaw":
+			received_data = float(msg[4:])
+			self.orientation_pub.publish(Float64(received_data))
+
+		elif msg[:5] == "Start":
+			self.button_pub.publish(String("Start"))
+		elif msg[:4] == "Drop":
+			self.button_pub.publish(String("Drop"))
+		elif msg[:6] == "Pickup":
+			self.button_pub.publish(String("Pickup"))
+
 		print msg
 		c.close()
 		return msg
@@ -50,4 +63,4 @@ class EchoSocket(object):
 if __name__ == "__main__":
 	es = EchoSocket()
 	while True:
-		es.read_socket(16)
+		es.read_socket(32)
